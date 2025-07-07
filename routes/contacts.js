3992c3d83550e42907c13f97d8776769f28dca21
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-07-07 12:09:52 -0300

    Create GET contact and routes to displey info

---
+++ routes/contacts.js
@@ -0,0 +1,10 @@
+// routes/contact.js 
+
+const express = require("express");
+const router = express.Router();
+const contactController = require('../controllers/contacts');
+
+router.get('/', contactController.getAll);
+router.get('/:id', contactController.getSingle);
+
+module.exports = router;
